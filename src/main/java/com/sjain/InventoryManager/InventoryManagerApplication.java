921a51f918431c6b611ad66d16f856e2cc02ea6d
Saksham Jain <jainsaksham67@gmail.com>
2023-12-06 17:57:48 +0530

    Initial commit

---
+++ src/main/java/com/sjain/InventoryManager/InventoryManagerApplication.java
@@ -0,0 +1,13 @@
+package com.sjain.InventoryManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryManagerApplication.class, args);
+	}
+
+}
