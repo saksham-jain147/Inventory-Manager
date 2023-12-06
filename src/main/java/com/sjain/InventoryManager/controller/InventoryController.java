3f3e5c875825d3eb403da98800da98acc64c8456
Saksham Jain <123298939+saksham-jain147@users.noreply.github.com>
2023-12-07 01:40:37 +0530

    Update InventoryController.java

--- src/main/java/com/sjain/InventoryManager/controller/InventoryController.java
+++ src/main/java/com/sjain/InventoryManager/controller/InventoryController.java
@@ -14,31 +14,32 @@ public class InventoryController {
 
     @Autowired
     private ProductsService productService;
-    @GetMapping("/home")
-    public String home(){
-        return "Hi there!";
-    }
-
+    
+    //Get a list of all products.
     @GetMapping("/products")
     public List<Product> getProducts(){
         return this.productService.getProducts();
     }
 
+    //Get a product by its ID.
     @GetMapping("/products/{product_id}")
     public Product getProduct(@PathVariable int product_id){
         return this.productService.getProduct(product_id);
     }
 
+    //Add a new product.
     @PostMapping("/products")
     public Product addProduct(@RequestBody Product product){
         return this.productService.addProduct(product);
     }
 
+    //Update an existing product.
     @PutMapping("/products")
     public Product updateProduct(@RequestBody Product product){
         return this.productService.updateProduct(product);
     }
 
+    //Delete a product by its ID.
     @DeleteMapping("/products/{product_id}")
     public ResponseEntity<HttpStatus> deleteProduct(@PathVariable int product_id){
         try{
